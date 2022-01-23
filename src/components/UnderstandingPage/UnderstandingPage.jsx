a2e8ba2960c9eaf782d68fff69e20fbf04fe5657
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-23 00:50:08 -0600

    created back button and link to support page.

--- src/components/UnderstandingPage/UnderstandingPage.jsx
+++ src/components/UnderstandingPage/UnderstandingPage.jsx
@@ -1,48 +1,60 @@
-import { useState } from 'react'
-import { Link , useHistory } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import './UnderstandingPage.css';
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "./UnderstandingPage.css";
 
 function UnderstandingPage() {
-    const dispatch = useDispatch();
-    const feedback = useSelector((store) => store.feedback);
+  const dispatch = useDispatch();
+  const feedback = useSelector((store) => store.feedback);
 
-    const [understanding, setUnderstanding] = useState();
-    
-    const handleSubmit = (event) => {
-        event.preventDefault;
+  const [understanding, setUnderstanding] = useState(understandingState);
 
-        if (understanding === "") {
-            return alert('Please select a number');
-        } else {
-            dispatch({
-                type: "SET_UNDERSTANDING_RATING",
-                payload: { property: "understanding", value: understanding}
-            });
-        }
+  let understandingState;
 
-    };
-    
-    return (
-        <>
-        <h1>How are you understanding the content today?</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="number"
-            variant="filled"
-            placeholder="1-5"
-            min="1"
-            max="5"
-            value={understanding}
-            // Changes string to number value on submit
-            onChange={(event) => setUnderstanding(Number(event.target.value))}
-          />
-          <Link to="/support">
-            <button onClick={handleSubmit}>Next</button>
-          </Link>
-        </form>
-      </>
-    )
+  if (feedback.feeling) {
+    console.log("understanding is", feedback.understanding);
+    understandingState = feedback.understanding;
+  } else {
+    console.log("understanding is undefined");
+    understandingState = "";
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault;
+
+    if (understanding === "") {
+      return alert("Please select a number");
+    } else {
+      dispatch({
+        type: "SET_UNDERSTANDING_RATING",
+        payload: { property: "understanding", value: understanding },
+      });
+    }
+  };
+
+  return (
+    <>
+      <h1>How well are you understanding the content today?</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          variant="filled"
+          placeholder="1"
+          min="1"
+          max="5"
+          value={understanding}
+          // Changes string to number value on submit
+          onChange={(event) => setUnderstanding(Number(event.target.value))}
+        />
+        <Link to="/">
+          <button>Back</button>
+        </Link>
+        <Link to="/support">
+          <button onClick={handleSubmit}>Next</button>
+        </Link>
+      </form>
+    </>
+  );
 }
 
 export default UnderstandingPage;
