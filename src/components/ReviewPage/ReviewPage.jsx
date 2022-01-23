1cf7098dea6fbf148cf41a2a1e913699a92a0b19
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-23 03:44:38 -0600

    added links to feeling and understanding page.

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,13 +1,27 @@
 import './ReviewPage.css';
-import { useState } from 'react'
-import { Link , useHistory } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react';
+import { Link , useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
 function ReviewPage() {
-
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const feedback = useSelector(store => store.feedback);
     return (
+        <>
         <h2> Review Your Feedback </h2>
-        // Feedback scores and submit button go here.
+
+           <Link to="/">
+           <h3>Feeling: {feedback.feeling} </h3>
+           </Link> 
+
+           <Link to="/understanding">
+               <h3>Understanding: {feedback.understanding}</h3>
+           </Link>
+        
+       
+        </>
 
        
 
