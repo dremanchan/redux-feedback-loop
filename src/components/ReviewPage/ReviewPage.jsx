7898dbd117896194292349c145395435fefc8fdb
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-23 03:58:55 -0600

    moved buttons

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,41 +1,47 @@
-import './ReviewPage.css';
-import { useState } from 'react';
-import { Link , useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
+import "./ReviewPage.css";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 function ReviewPage() {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const feedback = useSelector(store => store.feedback);
-    return (
-        <>
-        <h2> Review Your Feedback </h2>
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const feedback = useSelector((store) => store.feedback);
+  return (
+    <>
+      <h2> Review Your Feedback </h2>
 
-           <Link to="/">
-           <h3>Feeling: {feedback.feeling} </h3>
-           </Link> 
+      <h3>
+        Feeling: {feedback.feeling}
+        <Link to="/">
+          <button>Change Rating</button>
+        </Link>
+      </h3>
 
-           <Link to="/understanding">
-               <h3>Understanding: {feedback.understanding} </h3>
-           </Link>
+      <h3>
+        Understanding: {feedback.understanding}
+        <Link to="/understanding">
+          <button>Change Rating</button>
+        </Link>
+      </h3>
 
-           <Link to="/support">
-               <h3>Support: {feedback.support} </h3>
-           </Link>
+      <h3>
+        Support: {feedback.support}
+        <Link to="/support">
+          <button>Change Rating</button>
+        </Link>
+      </h3>
 
-           <Link to="/comments">
-               <h3>Comments: {feedback.comments ? 
-               feedback.comments : 'No comments were made.'}
-               </h3>
-           </Link>
-        
-       
-        </>
-
-       
-
-    )
+      <h3>
+        Comments:{" "}
+        {feedback.comments ? feedback.comments : "No comments were made."}
+      </h3>
+      <Link to="/comments">
+        <button>Change Comments</button>
+      </Link>
+    </>
+  );
 }
 
 export default ReviewPage;
